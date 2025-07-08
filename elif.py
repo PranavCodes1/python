19fe55e52e0e77a8413085b08b98444186c3078d
Pranav Paralkar <pranavparalkar1@gmail.com>
2025-07-08 13:02:47 +0530

    codes

---
+++ elif.py
@@ -0,0 +1,8 @@
+animal = input("which animal is good for being a pet, dog,cat or lion? \n")
+
+if animal == "dog" or  animal == "cat":
+    print("it's good for petting")
+elif animal == "lion":
+    print("it's not a good animal for petting")
+else:
+    print("I dont know about this animal")
---
+++ loop_ex2.py
@@ -0,0 +1,9 @@
+counter = 1
+
+while counter<5:
+   print("Hello")
+
+   counter += 1
+   print(f"the counter is now {counter}")
+
+print("All done with the loop")  
